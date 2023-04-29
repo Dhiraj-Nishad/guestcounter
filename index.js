29a57d7804e4e519273b2ca9ac1d72ac602f53da
Dhiraj-Nishad <dhirajnishad29@gmail.com>
2023-04-29 15:03:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let counts = 0;
+let numb2 = 0;
+function addoneplus() {
+    let count =  document.getElementById("addone");
+    counts = counts+1;
+    addone.innerHTML=counts;
+}
+function addoneplustwo() {
+    let count =  document.getElementById("addone");
+    counts = counts+2;
+    addone.innerHTML=counts;
+}
+function addoneplusthree() {
+    let count =  document.getElementById("addone");
+    counts = counts+3;
+    addone.innerHTML=counts;
+}
+
+function addtwoplus() {
+    let count =  document.getElementById("addtwo");
+    numb2 = numb2+1;
+    addtwo.innerHTML=numb2;
+}
+function addtwoplustwo() {
+    let count =  document.getElementById("addtwo");
+    numb2 = numb2+2;
+    addtwo.innerHTML=numb2;
+}
+function addtwoplusthree() {
+    let count =  document.getElementById("addtwo");
+    numb2 = numb2+3;
+    addtwo.innerHTML=numb2;
+}
+function addtwoplusthree() {
+    let count =  document.getElementById("addtwo");
+    numb2 = numb2+3;
+    addtwo.innerHTML=numb2;
+}
+
+// 
+function subtwoplus() {
+    let count =  document.getElementById("addtwo");
+    numb2 = numb2-1;
+    addtwo.innerHTML=numb2;
+}
+function subtwoplustwo() {
+    let count =  document.getElementById("addtwo");
+    numb2 = 0;
+    addtwo.innerHTML=numb2;
+}
+
+// 
+function subone() {
+    let count =  document.getElementById("addone");
+    counts = counts - 1;
+    addone.innerHTML=counts;
+}
+function subclear() {
+    let count =  document.getElementById("addone");
+    counts = 0;
+    addone.innerHTML=counts;
+}
+
+// 
+
